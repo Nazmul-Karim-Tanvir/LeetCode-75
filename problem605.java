93dd83d9198f34d6d42bac09a734cf215f2feb7a
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-02-03 23:51:23 +0600

    few update on 605

--- problem605.java
+++ problem605.java
@@ -1,15 +1,15 @@
 public class problem605 {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count =0;
-        for(int i=0; i<flowerbed.length; i++){
-            if ((flowerbed[i] == 0) && (i==0 || flowerbed[i-1] == 0) &&
-             ((i==flowerbed.length-1) || (flowerbed[i+1]==0)) ){
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if ((flowerbed[i] == 0) && (i == 0 || flowerbed[i - 1] == 0)
+                    && ((i == flowerbed.length - 1) || (flowerbed[i + 1] == 0))) {
                 flowerbed[i] = 1;
-                count ++;
+                count++;
                 i++;
             }
         }
-        return count>=n;
+        return count >= n;
     }
 
     public static void main(String[] args) {
