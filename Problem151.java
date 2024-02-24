80395fbe21ffac0e3bbb4350b24645bc29b3894b
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-02-24 23:45:49 +0600

    completed 151

--- Problem151.java
+++ Problem151.java
@@ -3,21 +3,10 @@ import java.util.Scanner;
 public class Problem151 {
     public String reverseWords(String s) {
         String result = "";
-        String result1 = s.trim();
-        String arr[] = result1.split("\\s+");
+        String arr[] = s.trim().split("\\s+");
 
-        for (int i = 0; i < arr.length / 2; i++) {
-            String temp = arr[i];
-            arr[i] = arr[arr.length - 1 - i];
-            arr[arr.length - 1 - i] = temp;
-        }
-        System.out.println(arr.length);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (i != arr.length - 1) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i != 0) {
                 result += arr[i] + " ";
             } else {
                 result += arr[i];
