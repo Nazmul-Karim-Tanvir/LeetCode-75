6f01a842cbaa6c32f58673fa2e03751923c62e51
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-04-18 14:40:49 +0600

    start

--- Problem334.java
+++ Problem334.java
@@ -23,24 +23,7 @@ class Problem334 {
         }
         return result;
     }
-
-    // Initial Approach
-    // public boolean increasingTriplet(int[] nums) {
-    // int n = nums.length;
-    // // int result[] = new int[n];
-    // boolean result = false;
-    // for (int i = 0; i < n; i++) {
-    // for (int j = 0; j < n; j++) {
-    // for (int k = 0; k < n; k++) {
-    // if((i<j && j<k)&&(nums[i] < nums[j] && nums[j] < nums[k])){
-    // result = true;
-    // break;
-    // }
-    // }
-    // }
-    // }
-    // return result;
-    // }
+  
 
     public static void main(String[] args) {
         Problem334 pr = new Problem334();
