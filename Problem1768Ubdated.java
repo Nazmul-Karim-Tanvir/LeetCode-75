ff6ebdddb8a5147d4c41cbb5afc7e6a3bf2b7fae
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-05-18 20:49:51 +0600

    Problem1768 Ubdated

---
+++ Problem1768Ubdated.java
@@ -0,0 +1,36 @@
+public class Problem1768Ubdated {
+
+    static class Solution {
+        public String mergeAlternately(String word1, String word2) {
+
+            int Min = Math.min(word1.length(), word2.length());
+
+            StringBuilder resultSring = new StringBuilder();
+            int i = 0, j = 0;
+
+            while (i < Min && j < Min) {
+                resultSring.append(word1.charAt(i++));
+                resultSring.append(word2.charAt(j++));
+            }
+
+            boolean big = false;
+
+            if (word1.length() > word2.length()) {
+                big = true;
+
+            }
+            if (big) {
+                resultSring.append(word1.substring(Min));
+            } else {
+                resultSring.append(word2.substring(Min));
+            }
+
+            return resultSring.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.mergeAlternately("pqr", "ABCDE"));
+    }
+}
