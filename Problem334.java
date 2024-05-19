0916eb67f726648f51bcfd2b3c491286d14443db
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-05-19 10:54:09 +0600

    problem 334 slight change

--- Problem334.java
+++ Problem334.java
@@ -1,32 +1,28 @@
 class Problem334 {
     public boolean increasingTriplet(int[] nums) {
-        boolean result = false;
+
         if (nums == null || nums.length < 3) {
-            result = false;
+            return false;
         }
 
-        int min = nums[0];
-        int position = 0;
-        int count = 0;
+        int firstMin = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < min) {
-                min = nums[i];
-                position = i; // Update the position when a smaller element is found
-            }
-            if (i > position && nums[i] > min) {
-                count++;
+        for (int num : nums) {
+            if (num <= firstMin) {
+                
+                firstMin = num;
+            } else if (num <= secondMin) {
+                secondMin = num;
+            } else {
+                return true;
             }
         }
-        if (count >= 3) {
-            result = true;
-        }
-        return result;
+        return false;
     }
-  
 
     public static void main(String[] args) {
         Problem334 pr = new Problem334();
-        System.out.println(pr.increasingTriplet(new int[] { 5,4,3,2,1 }));
+        System.out.println(pr.increasingTriplet(new int[] { 5, 4, 3, 2, 1 }));
     }
 }
