07a0b256bdaac19b683a60431926f9d9c3d5a501
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-05-22 16:36:08 +0600

    hi

--- Problem151Updated.java
+++ Problem151Updated.java
@@ -3,21 +3,30 @@ public class Problem151Updated {
         int n = s.length();
         StringBuilder result = new StringBuilder();
         int i = n - 1;
-
         while (i >= 0) {
             // Skip spaces
-            while (i >= 0 && s.charAt(i) == ' ') i--;
-            if (i < 0) break;
-            
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            if (i < 0) {
+                break;
+            }
+
             // Find the end of the word
             int j = i;
-            while (i >= 0 && s.charAt(i) != ' ') i--;
-            
+            while (i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+
+            // Add space after a word no space before 1st word 
+            if(result.length() > 0){
+                result.append(' ');
+            }
+
             // Append the word
-            if (result.length() > 0) result.append(' ');
-            result.append(s, i + 1, j + 1);
+            result.append(s,i+1,j+1);
         }
-        
+
         return result.toString();
 
     }
