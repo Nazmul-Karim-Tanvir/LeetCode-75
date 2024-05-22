df1bc8d5e42c5f1a8cff7b2d5ea41994e3a7608f
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-05-22 20:25:07 +0600

    done problem443

--- Problem443.java
+++ Problem443.java
@@ -1,10 +1,34 @@
 public class Problem443 {
     public int compress(char[] chars) {
-        return 0;
+        int index = 0;
+        int i = 0;
+
+        while (i<chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            // Count occurrences of the current character 
+            while (i<chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+
+            //Write the character to the array
+            chars[index++] = currentChar; 
+            
+            // Write the count to the array if more that 1
+            if(count > 1){
+                for(char c: Integer.toString(count).toCharArray()){
+                    chars[index++] = c;
+                }
+            }
+
+        }
+        return index;
     }
 
     public static void main(String[] args) {
         Problem443 pr = new Problem443();
-        System.err.println(pr.compress(null));
+        System.err.println(pr.compress(new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }));
     }
 }
