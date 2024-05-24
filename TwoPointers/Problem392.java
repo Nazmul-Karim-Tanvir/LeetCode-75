1eb99c655de783aaf4d040535026a8054f31e181
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-05-24 11:46:13 +0600

    done 392

--- TwoPointers/Problem392.java
+++ TwoPointers/Problem392.java
@@ -2,20 +2,33 @@ package TwoPointers;
 
 public class Problem392 {
     public boolean isSubsequence(String s, String t) {
-        int i = 0, j = 0;
-        while (i < s.length() && j < t.length()) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
-            }
-            j++;
+        if (s.isEmpty()) {
+            return true; // An empty string is a subsequence of any string
         }
-        return i == s.length();
+
+        int sIndex = 0, tIndex = 0;
+
+        // Traverse the sring t with tIndex
+        while (tIndex < t.length()) {
+            // if characters match in s are matched return true
+            if (t.charAt(tIndex) == s.charAt(sIndex)) {
+                sIndex++;
+                // if all characters in s are matched , return true
+                if (sIndex == s.length()) {
+                    return true;
+                }
+            }
+            // Always move to the next character in t
+            tIndex++;
+        }
+        // Not all characters in s are matched
+        return false; 
     }
 
     public static void main(String[] args) {
-        Problem392 pr =  new Problem392();
+        Problem392 pr = new Problem392();
         System.out.print(pr.isSubsequence("abc", "ahbgdc"));
-        System.out.println(pr.isSubsequence("axc", "ahbgdc"));
+        System.out.print(pr.isSubsequence("axc", "ahbgdc"));
     }
-    
+
 }
