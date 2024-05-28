858a21b9e6259c2d5bfb36e2c5e85666f2050080
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-05-28 13:17:12 +0600

    done 1456

--- SlidingWindow/Problem1456.java
+++ SlidingWindow/Problem1456.java
@@ -2,6 +2,10 @@ package SlidingWindow;
 
 public class Problem1456 {
     public int maxVowels(String s, int k) {
+        if (s == null || k == 0 || k > s.length()) {
+            return 0;
+        }
+
         boolean isVowel[] = new boolean[128];
         isVowel['a'] = true;
         isVowel['e'] = true;
@@ -12,22 +16,28 @@ public class Problem1456 {
         int maxCount = 0;
         int currentCount = 0;
 
-        for (int i = 0; i < isVowel.length; i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (isVowel[s.charAt(i)]) {
                 currentCount++;
             }
+
+            if (i >= k && isVowel[s.charAt(i - k)]) {
+                currentCount--;
+            }
+
+            if (i >= k - 1) {
+                maxCount = Math.max(maxCount, currentCount);
+            }
         }
 
-        return 0;
-
+        return maxCount;
     }
 
     public static void main(String[] args) {
         Problem1456 pr = new Problem1456();
-        System.err.println(pr.maxVowels(null, 0));
-        // System.err.println(pr.maxVowels("abciiidef", 2)); // output: 3
-        // System.out.println(pr.maxVowels("aeiou", 2)); // Output: 2
-        // System.out.println(pr.maxVowels("leetcode", 3)); // Output:2
-
+        System.err.println(pr.maxVowels(null, 0)); // Output: 0
+        System.err.println(pr.maxVowels("abciiidef", 3)); // Output: 3
+        System.out.println(pr.maxVowels("aeiou", 2)); // Output: 2
+        System.out.println(pr.maxVowels("leetcode", 3)); // Output: 2
     }
 }
