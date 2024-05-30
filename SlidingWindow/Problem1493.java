31ddb8a5f6960a21079a4831e3f0d3e0009addea
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-05-30 16:31:43 +0600

    completed 1493

--- SlidingWindow/Problem1493.java
+++ SlidingWindow/Problem1493.java
@@ -1,5 +1,30 @@
 package SlidingWindow;
 
 public class Problem1493 {
-    
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int zeroCount = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        Problem1493 pr = new Problem1493();
+        System.out.println(pr.longestSubarray(new int[]{0,1,1,1,0,1,1,0,1}));
+        System.out.println("m");
+    }
 }
