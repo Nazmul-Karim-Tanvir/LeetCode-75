7fd73bc57e9b546b4756c85aa942aa3f191cd536
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-06-20 19:05:01 +0600

    working on problem 1657

---
+++ HashMapSet/Problem1657.java
@@ -0,0 +1,12 @@
+package HashMapSet;
+
+public class Problem1657 {
+    public boolean closeStrings(String word1, String word2) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1657 problem = new Problem1657();
+        System.out.println(problem.closeStrings("abc", "bca"));
+    }
+}
