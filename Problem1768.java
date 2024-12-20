493b9ba1cc05717bdace556fa7db4f09c2506504
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-12-20 19:04:00 +0600

    pr 1768 updated

--- Problem1768.java
+++ Problem1768.java
@@ -4,22 +4,15 @@ public class Problem1768 {
         public String mergeAlternately(String word1, String word2) {
             String result = "";
             int min = Math.min(word1.length(), word2.length());
-            int big = 0;
+
             for (int i = 0; i < min; i++) {
                 result += word1.charAt(i);
                 result += word2.charAt(i);
             }
             if (word1.length() > word2.length()) {
-                big = word1.length();
-                for (int i = min; i < big; i++) {
-                    result += word1.charAt(i);
-                }
-            }
-            if (word2.length() > word1.length()) {
-                big = word2.length();
-                for (int i = min; i < big; i++) {
-                    result += word2.charAt(i);
-                }
+                result += word1.substring(word1.length());
+            } else {
+                result += word1.substring(word2.length());
             }
 
             return result;
